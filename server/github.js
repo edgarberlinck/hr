b09ae834fe0987fc7ed223b188855f54131d02cc
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2020-08-12 04:33:10 -0300

    wip

--- server/github.js
+++ server/github.js
@@ -9,30 +9,32 @@ const getProjectOppenedIssues = async repoUrl => {
     const issues = await client.get(`/repos${pathname}/issues`)
       .then(response => response.data)
       .then(issues => issues.filter(issue => issue.state === 'open'))
-      const dateToDays = (data) => {
-        try {
-          const initialData = moment(data)
-          return moment().diff(initialData) / (1000*60*60*24)
-        } catch {
-          return 0
-        }
-      }
-      const totalOpenedIssues = issues.length
+
+      return issues
+      // const dateToDays = (data) => {
+      //   try {
+      //     const initialData = moment(data)
+      //     return moment().diff(initialData) / (1000*60*60*24)
+      //   } catch {
+      //     return 0
+      //   }
+      // }
+      // const totalOpenedIssues = issues.length
       
-      const daysOpenedArray = issues.map(({ created_at }) => dateToDays(created_at))
+      // const daysOpenedArray = issues.map(({ created_at }) => dateToDays(created_at))
       
-      const avg = Math.round(daysOpenedArray.reduce((acc, current) => acc += current, 0) / totalOpenedIssues)
+      // const avg = Math.round(daysOpenedArray.reduce((acc, current) => acc += current, 0) / totalOpenedIssues)
       
-      const squareDiffs = daysOpenedArray.map(value => {
-        const diff = value - avg
-        const sqrDiff = diff * diff
-        return sqrDiff
-      })
-      .reduce((acc, current) => acc += current, 0)
+      // const squareDiffs = daysOpenedArray.map(value => {
+      //   const diff = value - avg
+      //   const sqrDiff = diff * diff
+      //   return sqrDiff
+      // })
+      // .reduce((acc, current) => acc += current, 0)
       
-      const stdDev = Math.round(Math.sqrt(squareDiffs / totalOpenedIssues))
+      // const stdDev = Math.round(Math.sqrt(squareDiffs / totalOpenedIssues))
       
-      return { totalOpenedIssues, avg, stdDev }
+      // return { totalOpenedIssues, avg, stdDev }
 
   } catch (err) {
     throw new Error(err)
--- test/github.spec.js
+++ test/github.spec.js
@@ -1,8 +1,22 @@
 const github = require('../server/github')
 const chai = require('chai')
+const sinon = require('sinon')
 
 describe('Project Issues List', () => {
   it('should retrieve only opened issues', () => {
-     
+    const expectedResponse = {}
+    
+    const res = {
+      send: sinon.spy()
+    };
+    const aStub = sinon.stub(axios, "get").resolves(Promise.resolve(expectedResponse));
+
+    const req = httpMock.createRequest({method:"get", url:"/GetInfo"});
+
+    info.GetInfo(req, res);
+
+    // At this point, I need to evaluate the response received (which should be expectedResponse)
+    assert(res.send.calledWith(expectedResponse)); 
+
   })
 })
